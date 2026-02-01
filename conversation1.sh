bc80be59b17d0a223daddb9314e3d89f46c30af3
mdsubhani231-nrt <mdsubhani231@gmail.com>
2026-02-01 22:54:06 +0530

    actions

--- conversation1.sh
+++ conversation1.sh
@@ -4,3 +4,4 @@ echo "suresh:: Hello  Ramesh , how are you?"
 echo "Ramesh:: i am fine , thanks for asking .how are you doing?"
 echo "suresh:: doing fine , what r u learning?"
 echo "Ramesh::i am learning shell .what are you?"
+echo "suresh :: " $PWD
